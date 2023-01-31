6302a1b331e7cc6d4f688b45bce291076f86c479
LeMustache <eaglehungergamer@gmail.com>
2023-01-31 17:27:02 +0100

    m

--- javascript/get-name.js
+++ javascript/get-name.js
@@ -1,20 +1,25 @@
-if (window.XMLHttpRequest) {
-    xmlhttp = new XMLHttpRequest();
-} else {
-xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+
+
+$.getJSON("../database/names-sa.json",function(data){
+    console.log(data[0])
+
+    dataInd = Math.floor(Math.random() * data.length)
+    console.log(data[dataInd])
+
+    document.getElementById("nameTitleArb").innerHTML = data[dataInd].name
+})
+
+function translateName(){
+    lang = document.querySelector('input[name="lang"]:checked').value;
+
+    $.getJSON("../database/names-"+lang+".json",function(data){
+        document.getElementById("nameTitle").innerHTML = data[dataInd].index+" "+data[dataInd].name
+        document.getElementById("nameDef").innerHTML = data[dataInd].text
+    })
 }
 
-// Get Arabic script
-xmlhttp.open("GET", "database/names-tur.xml", false);
-xmlhttp.send();
-xmlDoc = xmlhttp.responseXML;
+translateName()
 
-var names = xmlDoc.getElementsByTagName("names"); // get nammes
 
-// GET RANDOM NAME
-namesInd = Math.floor(Math.random() * names.length);
-chosenName =  xmlDoc.getElementsByTagName("name")
-
-console.log(chosenName)
 
 
